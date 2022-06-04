30ed829dde85bfa262ac4ca74d59db0a181c0593
Takahiro Iizuka <agdjgt@gmail.com>
2022-06-04 12:16:40 +0900

    Modifiled seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,42 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
   
-20.times do |u|
-  @user = User.create!(
-    name: "user#{u + 1}",
-    email: "user#{u + 1}@gmail.com",
-    password: "123456",
-    admin: false
+@user = User.create!(
+  name: "Daniel",
+  email: "daniel@gmail.com",
+  password: "123456",
+  admin: false
+)
+
+@admin_user = User.create!(
+  name: "Smith",
+  email: "smith@gmail.com",
+  password: "123456",
+  admin: true
+)
+
+
+
+50.times do |n|
+  date = Date.today+n+1
+  Task.create!(
+    title: "sample task#{n + 1}",
+    content: "sample content#{n + 1} by Kevin",
+    deadline_on: date,
+    priority: rand(3),
+    status: rand(3),
+    user_id: @user.id
   )
 end
 
+50.times do |n|
+  date = Date.today+n+1
+  Task.create!(
+    title: "sample task#{n + 1}",
+    content: "sample content#{n + 1} by Kevin",
+    deadline_on: date,
+    priority: rand(3),
+    status: rand(3),
+    user_id: @admin_user.id
+  )
+end
