5b3d77b18e97dae6e8efcb5ec2e0c7d4ed906ae7
Takahiro Iizuka <agdjgt@gmail.com>
2022-06-04 23:26:06 +0900

    Modified user model validation to case sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, length: { minimum: 6 }
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 
   private
 
